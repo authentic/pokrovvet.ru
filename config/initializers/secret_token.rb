4dc226fd2800e8ba165a977098545b4c5f30518d
unknown <Olga@.(none)>
2011-05-26 16:47:27 -0400

    pokrovvet files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokrovvetRu::Application.config.secret_token = 'be5f340d0b37a8f4a133bf3d74396c20873c1b18047b6396752beaa0b4a516466fd8e3a21420feade56d728b5f64099fb601e31963527af394b30466154552ad'
