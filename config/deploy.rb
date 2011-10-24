1a8ae0f550258f6952aec3772ac42882c9f814d6
Olga Hauze <galaxy73rambler@yahoo.com>
2011-10-24 11:59:59 -0400

    pokrovvet files

---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+require 'bundler/capistrano'
+
+
+
+#############################################################
+#	Application
+#############################################################
+set :application, "pokrovvet.ru"
+set :deploy_to, "/home/townofma/#{application}"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+set :use_sudo, false
+ssh_options[:port] = 7822
+ssh_options[:keys] = %w(~/.ssh/id_dsa)
+set :scm_verbose, true
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "townofma"
+set :domain, "townofma.www65.a2hosting.com"
+role :web, domain
+role :app, domain
+role :db, domain, :primary => true
+
+#############################################################
+#	Git
+#############################################################
+set :repository, "git@github.com:authentic/pokrovvet.ru.git"
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+#############################################################
+#	Passenger
+#############################################################
+
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
