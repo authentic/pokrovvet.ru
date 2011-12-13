820d27ad4be84b4402d65ad72e92c1ca010cab12
Olga Hauze <galaxy73rambler@yahoo.com>
2011-12-12 19:57:03 -0500

    Handling invalid signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
+# encoding: utf-8
 class SessionsController < ApplicationController
   layout 'public'
+
   def new
 
   end
-  def create
 
+  def create
+    public_user= PublicUser.authenticate(params[:session][:email], params[:session][:password])
+
+    if public_user.nil?
+      flash.now[:error] = "Нужно ввести правильный адрес электронной почты и пароль."
+      render 'new'
+    else
+      #handle successful sighin
+    end
   end
+
   def destroy
 
   end
