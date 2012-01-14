9bb2d6c880482a97397f270b35f983576953ff3f
Olga Hauze <galaxy73rambler@yahoo.com>
2012-01-13 20:55:32 -0500

    Asset pipeline

--- app/models/public_user.rb
+++ app/models/public_user.rb
@@ -57,14 +57,18 @@ class PublicUser < ActiveRecord::Base
 end
 
 
+
 # == Schema Information
 #
 # Table name: public_users
 #
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean(1)      default(FALSE)
 #
 
